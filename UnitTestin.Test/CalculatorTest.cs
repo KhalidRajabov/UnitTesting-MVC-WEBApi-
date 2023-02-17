a470ba21051747f840182b493588146bf5638d73
Khalid Radjabov <rammkhalid@gmail.com>
2023-02-17 22:14:46 +0400

    Add project files.

---
+++ UnitTestin.Test/CalculatorTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTesting.App;
+using Xunit;
+
+namespace UnitTestin.Test
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            /*//Arrange --- In arrange, variables that will take part in methods params must be initialized
+            int a = 5; 
+            int b =20;
+            var calculator = new Calculator();
+
+            //Act --- In act section methods of class that must be tested will be executed
+            var total = calculator.Add(a, b);
+
+
+            //Assert --- Matching test results to our expectations
+            Assert.Equal<int>(25, total);*/
+
+            /*var names = new List<string>() {"John", "Doe","Noone" };
+
+            Assert.Contains(names, x => x == "Doe");*/
+
+            /*var regEx = "^dog";
+            Assert.Matches(regEx, "dog");*/
+            
+        }
+    }
+}
